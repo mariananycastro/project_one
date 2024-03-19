cf0595664ae5b4e6aac91098f975e62497d451e0
mariana castro <mariananycastro@gmail.com>
2024-03-19 12:13:12 -0300

    remove unused routes

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module App
     config.load_defaults 7.0
 
     config.active_job.queue_adapter = :sneakers
+
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+      app.routes_reloader.paths.delete_if {|path| path =~ /turbo/ }
+    }
   end
 end
