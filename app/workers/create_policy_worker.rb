cfe48d52454806f88dac38a23bcb6f82db15a59f
mariana castro <mariananycastro@gmail.com>
2024-03-19 16:00:49 -0300

    change worker

--- app/workers/create_policy_worker.rb
+++ app/workers/create_policy_worker.rb
@@ -2,9 +2,9 @@
 
 class CreatePolicyWorker
   include Sneakers::Worker
-  from_queue 'testing'
+  from_queue 'create_policy'
 
   def work(msg)
-    logger.info "hello #{msg}"
+    logger.info "Creating policy worker #{msg}"
   end
 end
