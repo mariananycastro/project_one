e8006be03d7df039925cbada70328d48ae67a8a0
mariana castro <mariananycastro@gmail.com>
2024-03-27 13:26:36 -0300

    Finish CreatePolicyWorker

--- app/workers/create_policy_worker.rb
+++ app/workers/create_policy_worker.rb
@@ -2,9 +2,22 @@
 
 class CreatePolicyWorker
   include Sneakers::Worker
-  from_queue 'create_policy'
+  QUEUE_NAME = 'create-policy'
+
+  from_queue QUEUE_NAME
 
   def work(msg)
-    logger.info "Creating policy worker #{msg}"
+    ActiveRecord::Base.connection_pool.with_connection do
+      attr = JSON.parse(msg).with_indifferent_access
+
+      Rails.logger.tagged("Creating policy worker #{attr[:insured_person][:document]}") do |logger|
+        new_policy = CreatePolicyService.execute(attr)
+        logger.info new_policy
+
+        return ack! if new_policy
+
+        reject!
+      end
+    end
   end
 end
