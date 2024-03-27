7d7e7e5410b74c1423eb80bdfc1e153e1a7f6b57
mariana castro <mariananycastro@gmail.com>
2024-03-27 13:26:22 -0300

    change sneakers config

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,9 +1,7 @@
 require 'sneakers'
 
 Sneakers.configure(
-  connection: Bunny.new(
-    hostname: 'rabbitmq'
-  ),
+  amqp: "amqp://guest:guest@rabbitmq:5672",
   heartbeat: 30,
   exchange: 'sneakers',
   exchange_type: 'direct'
